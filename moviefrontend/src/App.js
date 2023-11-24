63ec87ac745da39e8e5de275a2e4f69108f3011f
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-11-24 10:09:00 +0100

    Header added

--- moviefrontend/src/App.js
+++ moviefrontend/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      {/* Render the Header component */}
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
