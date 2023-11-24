56f190acca341c0439761ee0885a76b270c79ce7
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-11-24 10:24:08 +0100

    Searchbar added

--- moviefrontend/src/App.js
+++ moviefrontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- moviefrontend/src/Header.jsx
+++ moviefrontend/src/Header.jsx
@@ -1,33 +1,38 @@
+// Header.jsx
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+    <Navbar expand="lg" className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="#home">FISKET</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="/signin">log in</Nav.Link>
-            
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Toggle type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarColor03">
+          <Nav className="navbar-nav me-auto">
+            <Nav.Link className="nav-link active" href="#">Home
+              <span className="visually-hidden">(current)</span>
+            </Nav.Link>
+            <Nav.Link className="nav-link" href="#">Features</Nav.Link>
+            <Nav.Link className="nav-link" href="#">Pricing</Nav.Link>
+            <Nav.Link className="nav-link" href="#">About</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <form className="d-flex">
+            <input className="form-control me-sm-2" type="search" placeholder="Search" />
+            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
