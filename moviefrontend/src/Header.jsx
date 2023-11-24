7648e199a87479d4abff67a3c7456fbffe6486fb
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-11-24 10:54:31 +0100

    header contetns

--- moviefrontend/src/Header.jsx
+++ moviefrontend/src/Header.jsx
@@ -1,38 +1,48 @@
-// Header.jsx
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 
+
+
 const Header = () => {
   return (
-    <Navbar expand="lg" className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+    
+    <Navbar expand="lg" className="bg-body-tertiary justify-content-between" data-bs-theme="light">
+        <Navbar fixed="top" />
       <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          {/**Logo */}
+        <Navbar.Brand href="#">LOGO HERE</Navbar.Brand>
         <Navbar.Toggle type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="navbarColor03">
           <Nav className="navbar-nav me-auto">
-            <Nav.Link className="nav-link active" href="#">Home
-              <span className="visually-hidden">(current)</span>
-            </Nav.Link>
-            <Nav.Link className="nav-link" href="#">Features</Nav.Link>
-            <Nav.Link className="nav-link" href="#">Pricing</Nav.Link>
-            <Nav.Link className="nav-link" href="#">About</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              {/**search bar */}
+          <form className="d-flex">
+            <input className="form-control me-sm-2" type="search" placeholder="Search" />
+            {/**filters */}
+          <NavDropdown title="Filters" id="basic-nav-dropdown">
               <NavDropdown.Item href="#">Action</NavDropdown.Item>
               <NavDropdown.Item href="#">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
             </NavDropdown>
-          </Nav>
-          <form className="d-flex">
-            <input className="form-control me-sm-2" type="search" placeholder="Search" />
+               {/**Search button */}
             <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            <Navbar.Toggle />
+        
+            <Navbar.Text>
+                <button className="btn btn-secondary my-2 my-sm-0" type="submit">Signin</button>
+             </Navbar.Text>
+      
+            {/**   Sign in  */}
+         
           </form>
+          
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
