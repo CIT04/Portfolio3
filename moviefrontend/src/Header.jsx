c0f3e0ccf585c0d62b3e41b786e541d6bb90e0e2
mjvj2680 <martinjrgensen7@gmail.com>
2023-11-24 10:22:45 +0100

    updated header

--- moviefrontend/src/Header.jsx
+++ moviefrontend/src/Header.jsx
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
