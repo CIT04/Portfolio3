465de86b35998c4a95fc2f58396e7610c2b1a4b0
Nakli98 <113427971+Nakli98@users.noreply.github.com>
2023-11-29 14:02:27 +0100

    header fixed

--- moviefrontend/src/pages/Header.jsx
+++ moviefrontend/src/pages/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import CustomDropdown from './css/CustomDropdown';
+import CustomDropdown from './CustomDropdown';
 import { NavLink, useNavigate } from 'react-router-dom';
 import './css/HeaderStyle.css';
 import React, { useEffect, useState } from 'react';
