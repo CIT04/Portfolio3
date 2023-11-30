4ff87cc0e9df11e2b16c62857d3d41fd00d0a052
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-11-30 13:23:24 +0100

    Routing from cards to media

--- moviefrontend/src/components/MovieCard.jsx
+++ moviefrontend/src/components/MovieCard.jsx
@@ -4,18 +4,18 @@ import './css/MovieCard.css';
 import { NavLink } from 'react-router-dom';
 
 
-export function MovieCard({ mediaid, title, plot, poster, year }) {
+export function MovieCard({ title, plot, poster, year }) {
   return (
-    <NavLink to={"/media/"+mediaid} className="nav-link">
+    
     <Card>
       <Card.Img variant="top" src={poster} className="card-img-top" />
       <Card.Body>
-        <Card.Title><h5>{mediaid}{title}<h1/></h5></Card.Title>
+        <Card.Title><h5>{title}<h1/></h5></Card.Title>
         <Card.Text><h6>{plot}</h6></Card.Text>
         <Card.Text>{year}</Card.Text>
       </Card.Body>
     </Card>
-  </NavLink>
+
   );
 }
 
--- moviefrontend/src/components/MovieCarousel.jsx
+++ moviefrontend/src/components/MovieCarousel.jsx
@@ -3,10 +3,11 @@ import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MovieCard from './MovieCard';
+import { NavLink } from 'react-router-dom';
 
 function MovieCarousel({ medias }) {
   const visibleItems = 5; // Set the number of visible items
-
+ 
   // Function to chunk the array into sets of 'visibleItems'
   const chunkArray = (arr, size) =>
     Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
@@ -22,13 +23,14 @@ function MovieCarousel({ medias }) {
           <Row>
             {chunk.map((media, mediaIndex) => (
               <Col key={mediaIndex}>
+                <NavLink to={"/media/"+media.id} className="nav-link">
                 <MovieCard
-                  mediaid={media.id}
                   title={media.title}
                   plot={media.plot}
                   poster={media.poster}
                   year={media.year}
                 />
+                </NavLink>
               </Col>
             ))}
           </Row>
