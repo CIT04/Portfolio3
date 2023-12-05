ba05bbbea85e6a1ff0a7319f0cb49ab9357724e9
Maurits Puggaard <maurits.pug@gmail.com>
2023-12-05 14:14:38 +0100

    MovieCard Fix

--- moviefrontend/src/components/MovieCard.jsx
+++ moviefrontend/src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ export function MovieCard({ title, plot, poster, year }) {
     <Card>
       <Card.Img variant="top" src={poster} className="card-img-top" />
       <Card.Body>
-        <Card.Title><h5>{title}<h1/></h5></Card.Title>
+        <Card.Title><h5><b><center>{title}</center></b></h5></Card.Title>
         <Card.Text><h6>{plot}</h6></Card.Text>
         <Card.Text>{year}</Card.Text>
       </Card.Body>
