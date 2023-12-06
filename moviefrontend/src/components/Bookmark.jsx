1b11aa5f3a5976afb106bcbe745a4e1ef9b236fe
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-12-06 12:56:26 +0100

    Update Bookmark.jsx

--- moviefrontend/src/components/Bookmark.jsx
+++ moviefrontend/src/components/Bookmark.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 import "./css/bookmark.css";
+import BookmarkButton from "./BookmarkButton";
 
-const Bookmark = ({ media }) => (
-  <NavLink to={`/media/${media.id}`} className="nav-link">
-    <div className="bookmark">
-      <img src={media.poster} alt="dælens" />
-      <div className="bookmark-info">
-        <h2>{media.title}</h2>
-        {/* Display the bookmark time */}
-        <h2>{media.year}</h2>
-        {/* Add additional information here */}
-        <p>{media.description}</p>
+const Bookmark = ({ media }) => {
+  const bookmarkstatus = true;
+
+  return (
+    <NavLink to={`/media/${media.id}`} className="nav-link">
+      <div className="bookmark">
+        <img src={media.poster} alt="dælens" />
+        <div className="bookmark-info">
+          <h2>{media.title}</h2>
+          {/* Display the bookmark time */}
+          <h2>{media.year}</h2>
+          {/* Add additional information here */}
+          <p>{media.description}</p>
+        </div>
+        <div>
+            Time added: {media.time}
+          <BookmarkButton onBookmark={bookmarkstatus} m_id={media.id} />
+        </div>
       </div>
-    </div>
-  </NavLink>
-);
+    </NavLink>
+  );
+};
 
 export default Bookmark;
