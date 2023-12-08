726babaf9a488ccc54eda1f4f86f37db6feff2df
mjvj2680 <martinjrgensen7@gmail.com>
2023-12-08 12:42:15 +0100

    rating component fix

--- moviefrontend/src/components/RatingComponent.jsx
+++ moviefrontend/src/components/RatingComponent.jsx
@@ -1,53 +1,83 @@
-import React, { useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import UserContext from './UserContext';
 
-const RatingComponent = ({ userRating, onRatingChange, onSaveRating, onDeleteRating }) => {
-  const [showModal, setShowModal] = useState(false);
+const RatingComponent = ({ m_id }) => {
+  const [hoverRating, setHoverRating] = useState(0);
+  const [userRating, setUserRating] = useState(0);
+  const { userToken } = useContext(UserContext);
 
   const handleSaveRating = () => {
-    setShowModal(false);
-    onSaveRating();
+   
+    fetch('http://localhost:5001/api/localrating/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        M_id: m_id,
+        U_id: userToken.id,
+        LocalScore: userRating,
+      }),
+    })
+
+   
   };
 
   const handleDeleteRating = () => {
-    setShowModal(false);
-    onDeleteRating();
+    
+
+   
+    fetch('http://localhost:5001/api/localrating/delete', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        M_id: m_id,
+        U_id: userToken.id,
+      }),
+    })
+
+    
+  };
+
+  const handleStarHover = (rating) => {
+    setHoverRating(rating);
+  };
+
+  const handleStarClick = () => {
+    setUserRating(hoverRating);
+    // Optionally, you can call handleSaveRating here to save the rating immediately
   };
 
   return (
     <div className="rating-component">
-   
-      <Button onClick={() => setShowModal(true)}>Edit Rating</Button>
-
-      {/* Rating Modal */}
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Rating</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Group controlId="formRating">
-            <Form.Label>Select Rating:</Form.Label>
-            <Form.Control as="select" value={userRating} onChange={(e) => onRatingChange(Number(e.target.value))}>
-              {[...Array(11).keys()].map((value) => (
-                <option key={value} value={value}>
-                  {value}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSaveRating}>
-            Save Rating
-          </Button>
-          <Button variant="danger" onClick={handleDeleteRating}>
-            Delete Rating
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <div className="star-rating">
+        {[...Array(10)].map((_, index) => (
+          <span
+            key={index}
+            className="star"
+            style={{
+              fontSize: '1.5em',
+              color: index < (hoverRating || userRating) ? 'gold' : 'gray',
+            }}
+            onMouseEnter={() => handleStarHover(index + 1)}
+            onMouseLeave={() => handleStarHover(0)}
+            onClick={handleStarClick}
+          >
+            {index < (hoverRating || userRating) ? '\u2605' : '\u2606'}
+          </span>
+        ))}
+      </div>
+      {userToken&&<div>
+      <button variant="primary" className="bookmark-button" onClick={handleSaveRating}>
+        Save Rating
+      </button>
+      <button variant="danger" className="bookmark-button" onClick={handleDeleteRating}>
+        Delete Rating
+      </button>
+      </div>}
     </div>
   );
 };
