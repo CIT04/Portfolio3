550fa2f10b8cae8df81e9e3d0660b2ef5704c44f
Maurits Puggaard <maurits.pug@gmail.com>
2023-12-11 15:05:52 +0100

    HomePage impl. fix to margin issues on pages and routing issues

--- moviefrontend/src/components/MovieCarousel.jsx
+++ moviefrontend/src/components/MovieCarousel.jsx
@@ -4,11 +4,15 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MovieCard from './MovieCard';
 import { NavLink } from 'react-router-dom';
-import Trailer from "./Trailer";
 
 function MovieCarousel({ medias }) {
+  if (!medias || !medias.length) {
+    // Return early or provide a default behavior when medias is empty
+    return null; // Or render a loading state, a message, etc.
+  }
+
   const visibleItems = 5; // Set the number of visible items
- 
+
   // Function to chunk the array into sets of 'visibleItems'
   const chunkArray = (arr, size) =>
     Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
@@ -24,22 +28,20 @@ function MovieCarousel({ medias }) {
           <Row>
             {chunk.map((media, mediaIndex) => (
               <Col key={mediaIndex}>
-                <NavLink to={"/media/"+media.id} className="nav-link">
-                <MovieCard
-                  title={media.title}
-                  plot={media.plot}
-                  poster={media.poster}
-                  year={media.year}
-                />
+                <NavLink to={`/media/${media.id}`} className="nav-link">
+                  <MovieCard
+                    title={media.title}
+                    plot={media.plot}
+                    poster={media.poster}
+                    year={media.year}
+                  />
                 </NavLink>
-                
               </Col>
             ))}
           </Row>
         </Carousel.Item>
       ))}
     </Carousel>
-    
   );
 }
 
